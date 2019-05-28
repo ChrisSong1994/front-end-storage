1cc1b32b5211618c1d20fac96d5ba171836e6b4e
songjun <songjun@songjuns-iMac.local>
2019-05-29 00:25:54 +0800

    fix：文件目录

--- node/fileServer/src/index.js
+++ node/fileServer/src/index.js
@@ -30,10 +30,80 @@ class FileServer {
   //  请求处理
   request(req, res) {
     const { pathname } = url.parse(req.url)
-    const filePath = path.join(this.config.root, pathname)
+    let filepath = path.join(this.config.root, pathname)
+    if (pathname === '/') {
+      const rootPath = path.join(this.config.root, 'index.html')
+      try {
+        const indexStat = fs.statSync(rootPath);
+        if (indexStat) {
+          filepath = rootPath;
+        }
+      } catch (e) { }
+    }
+
+    fs.stat(filepath, (err, stats) => {
+      if (err) {
+        this.sendError('not found', req, res);
+        return;
+      }
+
+      // 判断是否是文件夹
+      // 文件夹返回文件列表
+      if (stats.isDirectory) {
+        let files = fs.readdirSync(filepath);
+        files = files.map(file => ({
+          name: file,
+          url: path.join(pathname, file)
+        }));
+        console.log(files)
+        let html = this.list()({
+          title: pathname,
+          files
+        });
+        res.setHeader("Content-Type", "text/html")
+        res.end(html)
+      } else {
+        this.sendFile(req, res, filepath, stats);
+      }
+
+    })
+  }
+
+  // 文件列表模板渲染 返回一个函数用来接受需要渲染的数据
+  list() {
+    let tmpl = fs.readFileSync(path.resolve(__dirname, 'template', 'list.html'), 'utf8');
+    return handlebars.compile(tmpl);
+  }
+
+  /**
+    * 发送文件
+    * @param {*} req 请求流
+    * @param {*} res 响应流
+    * @param {*} filepath 文件路径 
+    * @param {*} stats 文件信息
+    */
+   sendFile(req, res, filepath, stats) {
 
 
 
+
+
+
+    
 
   }
+
+  /**
+  * 错误处理
+  * @param {*} err 处理信息
+  * @param {*} req 请求流
+  * @param {*} res 响应流
+  */
+  sendError(err, req, res) {
+    res.statusCode = 500;
+    res.end(`${err.toString()}`);
+  }
 }
+
+const fileServer = new FileServer()
+fileServer.start()
